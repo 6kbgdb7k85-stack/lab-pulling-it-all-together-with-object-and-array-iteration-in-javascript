5975ce7a043499f283a4f5c8f511a505857e66c8
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-06-10 18:11:13 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,116 +1,332 @@
 function gameObject() {
-    return {
-        home: {
-            teamName: "Brooklyn Nets",
-            colors: ["Black", "White"],
-            players: {
-                "Alan Anderson": {
-                    number: 0,
-                    shoe: 16,
-                    points: 22,
-                    rebounds: 12,
-                    assists: 12,
-                    steals: 3,
-                    blocks: 1,
-                    slamDunks: 1,
-                },
-                "Reggie Evens": {
-                    number: 30,
-                    shoe: 14,
-                    points: 12,
-                    rebounds: 12,
-                    assists: 12,
-                    steals: 12,
-                    blocks: 12,
-                    slamDunks: 7,
-                },
-                "Brook Lopez": {
-                    number: 11,
-                    shoe: 17,
-                    points: 17,
-                    rebounds: 19,
-                    assists: 10,
-                    steals: 3,
-                    blocks: 1,
-                    slamDunks: 15,
-                },
-                "Mason Plumlee": {
-                    number: 1,
-                    shoe: 19,
-                    points: 26,
-                    rebounds: 12,
-                    assists: 6,
-                    steals: 3,
-                    blocks: 8,
-                    slamDunks: 5,
-                },
-                "Jason Terry": {
-                    number: 31,
-                    shoe: 15,
-                    points: 19,
-                    rebounds: 2,
-                    assists: 2,
-                    steals: 4,
-                    blocks: 11,
-                    slamDunks: 1,
-                },
-            },
+  return {
+    home: {
+      teamName: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        "Alan Anderson": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1,
         },
-        away: {
-            teamName: "Charlotte Hornets",
-            colors: ["Turquoise", "Purple"],
-            players: {
-                "Jeff Adrien": {
-                    number: 4,
-                    shoe: 18,
-                    points: 10,
-                    rebounds: 1,
-                    assists: 1,
-                    steals: 2,
-                    blocks: 7,
-                    slamDunks: 2,
-                },
-                "Bismack Biyombo": {
-                    number: 0,
-                    shoe: 16,
-                    points: 12,
-                    rebounds: 4,
-                    assists: 7,
-                    steals: 7,
-                    blocks: 15,
-                    slamDunks: 10,
-                },
-                "DeSagna Diop": {
-                    number: 2,
-                    shoe: 14,
-                    points: 24,
-                    rebounds: 12,
-                    assists: 12,
-                    steals: 4,
-                    blocks: 5,
-                    slamDunks: 5,
-                },
-                "Ben Gordon": {
-                    number: 8,
-                    shoe: 15,
-                    points: 33,
-                    rebounds: 3,
-                    assists: 2,
-                    steals: 1,
-                    blocks: 1,
-                    slamDunks: 0,
-                },
-                "Brendan Hayword": {
-                    number: 33,
-                    shoe: 15,
-                    points: 6,
-                    rebounds: 12,
-                    assists: 12,
-                    steals: 22,
-                    blocks: 5,
-                    slamDunks: 12,
-                },
-            },
+        "Reggie Evens": {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slamDunks: 7,
         },
-    };
+        "Brook Lopez": {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 15,
+        },
+        "Mason Plumlee": {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slamDunks: 5,
+        },
+        "Jason Terry": {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slamDunks: 1,
+        },
+      },
+    },
+    away: {
+      teamName: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        "Jeff Adrien": {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slamDunks: 2,
+        },
+        "Bismack Biyombo": {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slamDunks: 10,
+        },
+        "DeSagna Diop": {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slamDunks: 5,
+        },
+        "Ben Gordon": {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slamDunks: 0,
+        },
+        "Brendan Hayword": {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slamDunks: 12,
+        },
+      },
+    },
+  };
 }
+
+const gameData = gameObject();
+
+function findPlayer(playerName) {
+  const teams = [];
+  for (let team in gameData) {
+    teams.push(gameData[team]);
+  }
+  for (const team of teams) {
+    for (let name in team.players) {
+      if (name === playerName) {
+        return team.players[name];
+      }
+    }
+  }
+}
+
+function findTeam(teamName) {
+  for (let team in gameData) {
+    if (gameData[team].teamName === teamName) {
+      return gameData[team];
+    }
+  }
+}
+
+function numPointsScored(playerName) {
+  return findPlayer(playerName).points;
+}
+
+function shoeSize(playerName) {
+  return findPlayer(playerName).shoe;
+}
+
+function teamColors(teamName) {
+  return findTeam(teamName).colors;
+}
+
+function teamNames() {
+  const teamNames = [];
+  for (let team in gameData) {
+    teamNames.push(gameData[team].teamName);
+  }
+  return teamNames;
+}
+
+function playerNumbers(teamName) {
+  const team = findTeam(teamName);
+  const numbers = []; //named numbers instead of playerNumbers to avoid confusion/conflict with the function name
+  Object.keys(team.players).forEach((player) => {
+    numbers.push(team.players[player].number);
+  });
+  return numbers;
+}
+
+function playerStats(playerName) {
+  return findPlayer(playerName);
+}
+
+function bigShoeRebounds() {
+  let playerWithLargestShoe;
+  Object.keys(gameData).forEach((team) => {
+    Object.keys(gameData[team].players).forEach((playerName) => {
+      if (
+        !playerWithLargestShoe ||
+        playerWithLargestShoe.show < gameData[team].players[playerName].shoe
+      ) {
+        playerWithLargestShoe = gameData[team].players[playerName];
+      }
+    });
+  });
+  return playerWithLargestShoe.rebounds;
+}
+
+function mostPointsScored() {
+  let highestScoringPlayer;
+  Object.keys(gameData).forEach((team) => {
+    Object.keys(gameData[team].players).forEach((playerName) => {
+      if (
+        !highestScoringPlayer ||
+        highestScoringPlayer.points < gameData[team].players[playerName].points
+      ) {
+        highestScoringPlayer = {
+          name: playerName,
+          points: gameData[team].players[playerName].points,
+        };
+      }
+    });
+  });
+  return highestScoringPlayer.name;
+}
+
+function winningTeam() {
+  const teamScores = {};
+  let winningTeam;
+  Object.keys(gameData).forEach((team) => {
+    teamScores[gameData[team].teamName] = 0;
+    Object.keys(gameData[team].players).forEach((playerName) => {
+      teamScores[gameData[team].teamName] +=
+        gameData[team].players[playerName].points;
+    });
+  });
+  Object.keys(teamScores).forEach((team) => {
+    if (!winningTeam || teamScores[winningTeam] < teamScores[team]) {
+      winningTeam = team;
+    }
+  });
+  return winningTeam;
+}
+
+//helper function to join an array into a list following normal grammer for inserting commas and an 'and' in a list (1,2, and 3 or 1 and 2)
+function joinListWithClosingAnd(array) {
+  if (array.length === 1) {
+    return array[0];
+  }
+  if (array.length === 2) {
+    return array.join(" and ");
+  } else {
+    let formattedList = "";
+    for (let i = 0; i < array.length - 1; i++) {
+      formattedList += array[i] + ", ";
+    }
+    formattedList += "and " + array[array.length - 1];
+    return formattedList;
+  }
+}
+
+function getPlayersWithLongestName(playerNames) {
+  let isTied = false;
+  let longestName;
+  let longestNameLength;
+  playerNames.forEach((name) => {
+    if (!longestName || longestName.length < name.length) {
+      longestName = name;
+    } else if (longestName.length == name.length) {
+      isTied = true;
+      longestNameLength = name.length;
+    }
+  });
+  if (!isTied) {
+    return [longestName];
+  } else {
+    return playerNames.filter((name) => name.length === longestNameLength);
+  }
+}
+
+//helper function to get the list of all player names
+function getAllPlayerNames() {
+  const playerNames = [];
+  Object.keys(gameData).forEach((team) => {
+    Object.keys(gameData[team].players).forEach((playerName) => {
+      playerNames.push(playerName);
+    });
+  });
+  return playerNames;
+}
+
+function playerWithLongestName() {
+  const playerNames = getAllPlayerNames();
+  const longestName = getPlayersWithLongestName(playerNames);
+  if (longestName.length === 1) {
+    return longestName[0];
+  } else {
+    return (
+      joinListWithClosingAnd(longestName) + " are tied for the longest name"
+    );
+  }
+}
+
+function doesLongNameStealATon() {
+  const playerNames = getAllPlayerNames();
+  const longestNames = getPlayersWithLongestName(playerNames);
+  if (longestNames.length === 1) {
+    const stealCount = findPlayer(longestNames[0]).steals;
+    for (const team in gameData) {
+      for (const playerName in gameData[team].players) {
+        if (stealCount < gameData[team].players[playerName].steals) {
+          return false;
+        }
+      }
+    }
+    return true;
+  } else {
+    let stealCount;
+    let stealLeader; //stores the name of the player with the most steals out of the list of players tied for longest name
+    longestNames.forEach((playerName) => {
+      const player = findPlayer(playerName);
+      if (!stealCount || stealCount < player.steals) {
+        stealCount = player.steals;
+        stealLeader = playerName;
+      }
+    });
+    for (const team in gameData) {
+      for (const playerName in gameData[team].players) {
+        if (stealCount < gameData[team].players[playerName]) {
+          return false;
+        }
+      }
+    }
+    const longestNamesNotSteals = longestNames.filter(
+      (name) => name !== stealLeader,
+    ); //array for players that tied for longest name but not steals
+    return joinListWithClosingAnd(longestNamesNotSteals)+' doesn\'t have the most steals, but '+stealLeader+' does.'
+  }
+}
+
+console.log(numPointsScored("Alan Anderson"));
+console.log(shoeSize("Ben Gordon"));
+console.log(teamColors("Brooklyn Nets"));
+console.log(playerNumbers("Brooklyn Nets"));
+console.log(teamNames());
+console.log(playerStats("Alan Anderson"));
+console.log(bigShoeRebounds());
+console.log(mostPointsScored());
+console.log(winningTeam());
+console.log(playerWithLongestName());
+console.log(doesLongNameStealATon());
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,42 +1,49 @@
-describe('Basketball Stats', function () {
-  
-  describe('numPointsScored', function () {
-    it('should return points scored by a player', function () {
-      expect(numPointsScored('Alan Anderson')).to.equal(22);
-      expect(numPointsScored('Ben Gordon')).to.equal(33);
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+describe("Basketball Stats", function () {
+  describe("numPointsScored", function () {
+    it("should return points scored by a player", function () {
+      expect(numPointsScored("Alan Anderson")).to.equal(22);
+      expect(numPointsScored("Ben Gordon")).to.equal(33);
     });
   });
 
-  describe('shoeSize', function () {
-    it('should return shoe size of a player', function () {
-      expect(shoeSize('Alan Anderson')).to.equal(16);
-      expect(shoeSize('Ben Gordon')).to.equal(15);
-    });
-  });
-  
-  describe('teamColors', function () {
-    it('should return team colors', function () {
-      expect(teamColors('Brooklyn Nets')).to.deep.equal(['Black', 'White']);
-      expect(teamColors('Charlotte Hornets')).to.deep.equal(['Turquoise', 'Purple']);
-    });
-  });
-  
-  describe('teamNames', function () {
-    it('should return team names', function () {
-      expect(teamNames()).to.deep.equal(['Brooklyn Nets', 'Charlotte Hornets']);
+  describe("shoeSize", function () {
+    it("should return shoe size of a player", function () {
+      expect(shoeSize("Alan Anderson")).to.equal(16);
+      expect(shoeSize("Ben Gordon")).to.equal(15);
     });
   });
 
-  describe('playerNumbers', function () {
-    it('should return player numbers of a team', function () {
-      expect(playerNumbers('Brooklyn Nets')).to.deep.equal([0, 30, 11, 1, 31]);
-      expect(playerNumbers('Charlotte Hornets')).to.deep.equal([4, 0, 2, 8, 33]);
+  describe("teamColors", function () {
+    it("should return team colors", function () {
+      expect(teamColors("Brooklyn Nets")).to.deep.equal(["Black", "White"]);
+      expect(teamColors("Charlotte Hornets")).to.deep.equal([
+        "Turquoise",
+        "Purple",
+      ]);
     });
   });
 
-  describe('playerStats', function () {
-    it('should return stats of a player', function () {
-      expect(playerStats('Alan Anderson')).to.deep.equal({
+  describe("teamNames", function () {
+    it("should return team names", function () {
+      expect(teamNames()).to.deep.equal(["Brooklyn Nets", "Charlotte Hornets"]);
+    });
+  });
+
+  describe("playerNumbers", function () {
+    it("should return player numbers of a team", function () {
+      expect(playerNumbers("Brooklyn Nets")).to.deep.equal([0, 30, 11, 1, 31]);
+      expect(playerNumbers("Charlotte Hornets")).to.deep.equal([
+        4, 0, 2, 8, 33,
+      ]);
+    });
+  });
+
+  describe("playerStats", function () {
+    it("should return stats of a player", function () {
+      expect(playerStats("Alan Anderson")).to.deep.equal({
         number: 0,
         shoe: 16,
         points: 22,
@@ -44,10 +51,10 @@ describe('Basketball Stats', function () {
         assists: 12,
         steals: 3,
         blocks: 1,
-        slamDunks: 1
+        slamDunks: 1,
       });
 
-      expect(playerStats('Ben Gordon')).to.deep.equal({
+      expect(playerStats("Ben Gordon")).to.deep.equal({
         number: 8,
         shoe: 15,
         points: 33,
@@ -55,14 +62,79 @@ describe('Basketball Stats', function () {
         assists: 2,
         steals: 1,
         blocks: 1,
-        slamDunks: 0
+        slamDunks: 0,
       });
     });
   });
 
-  describe('bigShoeRebounds', function () {
-    it('should return number of rebounds for the player with the biggest shoe', function () {
+  describe("bigShoeRebounds", function () {
+    it("should return number of rebounds for the player with the biggest shoe", function () {
       expect(bigShoeRebounds()).to.equal(12);
+    });
+  });
+
+  describe("mostPointsScored", () => {
+    it("should return the name of the player who scored the most points", () => {
+      expect(mostPointsScored()).to.equal("Ben Gordon");
+    });
+  });
+
+  describe("winningTeam", () => {
+    it("should return the team with the most points scored", () => {
+      expect(winningTeam()).to.equal("Brooklyn Nets");
+    });
+  });
+
+  describe("playerWithLongestName", () => {
+    let sandbox;
+
+    beforeEach(() => {
+      sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+    it("should return the player(s) with the longest name", () => {
+      expect(playerWithLongestName()).to.equal(
+        "Bismack Biyombo and Brendan Hayword are tied for the longest name",
+      );
+    });
+    it("should return a single name if there is only one player with the longest name", () => {
+      sandbox
+        .stub(window, "getPlayersWithLongestName")
+        .returns(["Brendan Hayword"]); //mocking only 1 player having logest name
+      expect(playerWithLongestName()).to.equal("Brendan Hayword");
+    });
+  });
+
+  describe("doesLongNameStealATon", () => {
+    let sandbox;
+
+    beforeEach(() => {
+      sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it("should return false if the player with the longest name doesn't have the most steals", () => {
+      sandbox
+        .stub(window, "getPlayersWithLongestName")
+        .returns(["Alan Anderson"]); //One player with the longest name has the most steals so setting the mock to use another player for false test
+      expect(doesLongNameStealATon()).to.equal(false);
+    });
+    it("should return true if the player with the longest name has the most steals", () => {
+      sandbox
+        .stub(window, "getPlayersWithLongestName")
+        .returns(["Brendan Hayword"]); //has the most steals but ties longest name so mocking only his name being longest
+      expect(doesLongNameStealATon()).to.equal(true);
+    });
+    it("should return a message clarifying if there is a tie for longest name to show which player with the longest name had the most steals", () => {
+      expect(doesLongNameStealATon()).to.equal(
+        "Bismack Biyombo doesn't have the most steals, but Brendan Hayword does.",
+      );
     });
   });
 });
